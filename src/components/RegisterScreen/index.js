414b134dea9b31ee56752bda0c530ed5de6bfdaf
Het Shah <htshah60@gmail.com>
2019-06-21 15:49:06 +0530

    perf: Removed unused variablses

--- src/components/RegisterScreen/index.js
+++ src/components/RegisterScreen/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Grid, Typography } from "@material-ui/core";
+import { Grid } from "@material-ui/core";
 
 // Common components
 import { PageWrapper } from "../../common/Wrapper";
