b0bc1193b641f22969d7bb0376f6e6836707166e
Het Shah <htshah60@gmail.com>
2019-06-21 15:47:34 +0530

    feat: Added new links

--- src/common/Router.js
+++ src/common/Router.js
@@ -2,9 +2,9 @@ import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 import NotFound from "./NotFound";
-// import RegisterScreen from "../components/RegisterScreen";
+import RegisterScreen from "../components/RegisterScreen";
 import LoginScreen from "../components/LoginScreen";
-// import HomeScreen from "../components/LoginScreen";
+import HomeScreen from "../components/HomeScreen";
 
 import PrivateRoute from "./PrivateRoute";
 import GuestRoute from "./GuestRoute";
@@ -12,10 +12,10 @@ import GuestRoute from "./GuestRoute";
 const Router = () => (
   <BrowserRouter>
     <Switch>
-      {/* <GuestRoute exact path="/register" component={RegisterScreen} /> */}
       <GuestRoute exact path="/login" component={LoginScreen} />
+      <GuestRoute exact path="/register" component={RegisterScreen} />
 
-      {/* <PrivateRoute exact path="/home" component={HomeScreen} /> */}
+      <PrivateRoute exact path="/home" component={HomeScreen} />
       <Route component={NotFound} />
     </Switch>
   </BrowserRouter>
