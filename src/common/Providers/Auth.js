cae0b6612ab23d50cc6c56a243a26fd11135ab77
Het Shah <htshah60@gmail.com>
2019-06-21 16:06:50 +0530

    feat: created custom hook for Auth

--- src/common/Providers/Auth.js
+++ src/common/Providers/Auth.js
@@ -1,15 +1,24 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 import Cookies from "js-cookie";
+import jwtDecode from "jwt-decode";
 
 import ApiAuth from "../../api/Auth";
 
 const AuthContext = React.createContext();
 const userCookieName = "token";
 
+const isUserAdmin = () => {
+  const token = Cookies.get(userCookieName);
+  const payload = jwtDecode(token);
+  return payload["sub"] === "admin";
+};
+
 const AuthProvider = props => {
   const [isAuth, setAuth] = useState(
     typeof Cookies.get(userCookieName) !== "undefined"
   );
+
+  const [isAdmin, setAdmin] = useState(!isAuth ? false : isUserAdmin());
 
   const login = (email, password) => {
     return new Promise((resolve, reject) => {
@@ -48,6 +57,7 @@ const AuthProvider = props => {
     <AuthContext.Provider
       value={{
         isAuth: isAuth,
+        isAdmin: isAdmin,
         register: register,
         login: login,
         logout: logout,
@@ -60,5 +70,11 @@ const AuthProvider = props => {
 };
 
 const AuthConsumer = AuthContext.Consumer;
+
+const useAuth = () => {
+  const context = useContext(AuthContext);
+  return context;
+};
+
 export default AuthContext;
-export { AuthProvider, AuthConsumer };
+export { AuthProvider, AuthConsumer, useAuth };
