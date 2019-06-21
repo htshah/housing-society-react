c4dd107b722fc42f39c0193197e5245bfcdb4063
Het Shah <htshah60@gmail.com>
2019-06-21 15:48:02 +0530

    fix: Fix for side nav btn always shown

--- src/common/SimplePageGrid/index.js
+++ src/common/SimplePageGrid/index.js
@@ -31,7 +31,7 @@ export default function({
         {/* Nav Bar */}
         <Grid item>
           <NavBar
-            showSideNavBtn
+            showSideNavBtn={showSideNavBtn}
             onSideNavBtnClick={() => setSideBarVisible(true)}
             title={title}
           />
